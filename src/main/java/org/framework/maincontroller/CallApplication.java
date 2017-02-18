5119f533800c89a855b63c90aaecc61f8d63466f
Hemanth Sridhar <hemanthsridhar@Hemanths-MacBook-Pro.local>
2017-02-18 23:55:15 +0530

    added open application and maximize steps for browser window

--- src/main/java/org/framework/maincontroller/CallApplication.java
+++ src/main/java/org/framework/maincontroller/CallApplication.java
@@ -3,12 +3,24 @@ import org.framework.maincontroller.MainController;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import ru.yandex.qatools.allure.annotations.Step;
+
 public class CallApplication extends MainController{
 	
 	@Parameters(value={"Product Name","Application URL","browser","Remote Url"})
 	@BeforeMethod(alwaysRun=true)
 	public void callApp(String productName,String applicationURL,String browser,String remoteURL) {
+		openApplication(applicationURL);
+		maximize();
+	}
+	
+	@Step("Navigate to {0}")
+	public void openApplication(String applicationURL){
 		getDriver().get(applicationURL);
+	}
+	
+	@Step("Maximize the browser window")
+	public void maximize(){
 		getDriver().manage().window().maximize();
 	}
 }
